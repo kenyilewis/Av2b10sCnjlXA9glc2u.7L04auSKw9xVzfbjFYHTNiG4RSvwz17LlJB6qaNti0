e73527340a729ba84a2d441cf65d965f615e79b9
kenyilewis <kenyilewis@yahoo.com>
2024-06-29 03:33:07 +0200

    feat: Add auth module, Add useGlobalPipes in main

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+import { UserService } from '../user/application/user.service';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(userLogin: LoginDto) {
+    const { email, password } = userLogin;
+    const user = await this.userService.findUserToAuth(email);
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payloadJWT = { userId: user.id, email: user.email };
+    const accessToken = this.jwtService.sign(payloadJWT, { expiresIn: '1h' });
+
+    return {
+      id: user.id,
+      email: user.email,
+      accessToken,
+    };
+  }
+}
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,2 +1,3 @@
 export { DatabaseModule } from './database/database.module';
 export { UserModule } from './user/user.module';
+export { AuthModule } from './auth/auth.module';
